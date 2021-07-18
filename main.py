438430ba8ce597a90545b4797cc2abafb9f45b18
omar2535 <omar2535@gmail.com>
2021-07-18 10:25:55 -0700

    Update readme

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+from definitions import DEFAULT_TTL
 from utils.HostedZone import HostedZone
 from utils.Credentials import Credentials
 import boto3
 from requests import get
 
-print("Starting DHCP auto updater")
+print("Starting Dynamic route-53 auto updater")
 
 # Program start
 ip = get('https://api.ipify.org').text
 credentials = Credentials()
 hosted_zones = HostedZone()
+hosted_zone_id = hosted_zones.get_hosted_zone_id()
 
 session = boto3.Session(
-    aws_access_key_id=credentials.get_aws_access_key_id,
-    aws_secret_access_key=credentials.get_aws_secret_access_key
+    aws_access_key_id=credentials.get_aws_access_key_id(),
+    aws_secret_access_key=credentials.get_aws_secret_access_key()
 )
 
 route53_client = session.client('route53')
@@ -28,9 +30,13 @@ for record in hosted_zones.get_records():
     change['ResourceRecordSet'] = {}
     change['ResourceRecordSet']['Name'] = record['record_name']
     change['ResourceRecordSet']['Type'] = record['record_type']
+    change['ResourceRecordSet']['TTL'] = DEFAULT_TTL
     change['ResourceRecordSet']['ResourceRecords'] = [{'Value': ip}]
     change_batch['Changes'].append(change)
 
+response = route53_client.change_resource_record_sets(
+    HostedZoneId=hosted_zone_id,
+    ChangeBatch=change_batch
+)
 
-
-print("Complete")
+print("Dynamic IP updater complete")
--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
-# DHCP Route53 auto-updater
+# Dynamic IP Route53 auto-updater
 
-Script to auto-update route53 records to point to a new IP address. The script will be intended to be run as a monitoring job
-so that when the host IP changes, the route-53 record can automatically be changed to reflect that.
+Script to auto-update route53 records to point to a new IP address. The script is intended to be run periodically so that when the host IP changes, the route-53 record can automatically be changed to reflect that.
